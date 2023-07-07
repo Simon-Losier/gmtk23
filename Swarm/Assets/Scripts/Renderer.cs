ee00442782f1b19ab9f269bd46bb5509325ffa21
Aryah Kannan <aryah@dal.ca>
2023-07-07 16:06:50 -0300

    adjustments

--- Swarm/Assets/Scripts/Renderer.cs
+++ Swarm/Assets/Scripts/Renderer.cs
@@ -3,21 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Renderer : MonoBehaviour {
     [SerializeField] private int width = 10;
     [SerializeField] private int height = 10;
     private float vertical;
     [SerializeField] private Transform wall;
-    [SerializeField] private float length;
-
+    [SerializeField] private float gap;
+    private float length;
 
     public Vector3 GetNodeCenter(int i, int j) {
         if (i >= width || j >= height || i < 0 || j < 0) return Vector3.zero;
         
         // length = length * Mathf.Cos(30 * Mathf.Deg2Rad);
         // vertical = length + (length * Mathf.Sin(30 * Mathf.Deg2Rad));
-                
+        
         Vector3 pos = new Vector3((i - width / 2) * length, transform.position.y, (j - height / 2) * length); // center of node
                 
         // if (j % 2 == 0) {
@@ -38,7 +39,6 @@ public class Renderer : MonoBehaviour {
                 if (node.HasFlag(Generator.NodeState.Up)) {
                     Transform up = Instantiate(wall, transform);
                     
-                    // up.eulerAngles = new Vector3(0, -30, 0);
                     up.position = pos + new Vector3(0, 0, length / 2);
                     up.name = "upLeft" + name;
                     
@@ -47,7 +47,7 @@ public class Renderer : MonoBehaviour {
                 if (node.HasFlag(Generator.NodeState.Left)) {
                     Transform left = Instantiate(wall, transform);
                     
-                    // left.eulerAngles = new Vector3(0, 90, 0);
+                    left.eulerAngles = new Vector3(0, 90, 0);
                     left.position = pos + new Vector3(-length / 2, 0, 0);
                     left.name = "left" + name;
                 }
@@ -68,7 +68,7 @@ public class Renderer : MonoBehaviour {
                         Transform left = Instantiate(wall, transform);
                         
 
-                        // left.eulerAngles = new Vector3(0, 90, 0);
+                        left.eulerAngles = new Vector3(0, 90, 0);
                         left.position = pos + new Vector3(length / 2, 0, 0);
                         left.name = "right" + name;
                     }
@@ -77,10 +77,13 @@ public class Renderer : MonoBehaviour {
             }
         }    
     }
-    
+
+    private void Start() {
+    }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
+            length = wall.localScale.x + gap;
             
             foreach (Transform t in transform) {
                 if (t.name != "Ground") {
