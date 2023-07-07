63b62cd22b9bd0f16411fbb8061e0318078ac2b6
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2023-07-07 18:58:12 -0300

    added max speed variable

--- Swarm/Assets/Scripts/Swarm/Zombie.cs
+++ Swarm/Assets/Scripts/Swarm/Zombie.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Zombie : MonoBehaviour {
     [SerializeField] private Transform swarmLead;
     [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
     [SerializeField] private float stoppingDistance;
     private Rigidbody rb;
 
@@ -16,9 +17,11 @@ public class Zombie : MonoBehaviour {
 
     private void Update() {
         
-        transform.LookAt(new Vector3(swarmLead.position.x, 0, swarmLead.position.z));
-        if (Vector3.Distance(transform.position, new Vector3(swarmLead.position.x, 0, swarmLead.position.z)) > stoppingDistance) {
-            rb.AddForce(transform.forward * speed, ForceMode.Force);
+        transform.LookAt(new Vector3(swarmLead.position.x, transform.position.y, swarmLead.position.z));
+        if (Vector3.Distance(transform.position, new Vector3(swarmLead.position.x, transform.position.y, swarmLead.position.z)) > stoppingDistance) {
+            if (rb.velocity.magnitude < maxSpeed) {
+                rb.AddForce(transform.forward * speed, ForceMode.Force);
+            }
         }
         else {
             rb.velocity = Vector3.zero;
